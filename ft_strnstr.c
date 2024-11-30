8f7c56efeb0b3f3bb5153fd7a23e000644ba77cd
theo <theo.ubuntu42@gmail.com>
2024-11-30 13:41:08 +0100

    add ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	len_little;
+	size_t	i;
+	size_t	j;
+
+	if (!big || !little)
+		return (NULL);
+	if (ft_strlen(little) == 0)
+		return ((char *)big);
+	len_little = ft_strlen(little);
+	i = 0;
+	while (i < len && big[i] != '\0')
+	{
+		j = 0;
+		while (big[i + j] != '\0' && big[i + j] == little[j] && i + j < len)
+			j++;
+		if (len_little == j)
+			return ((char *) big + i);
+		i++;
+	}
+	return (NULL);
+}
