d39d72d1647d502e3f6fea2c33bfe88bc3709e47
theo <theo.ubuntu42@gmail.com>
2024-11-30 15:40:53 +0100

    add ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	int	len;
+	char	*result;
+	int	i;
+	int	j;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[i])
+	{
+		result[i] = s2[j];
+		i++;
+		j++;
+	}
+	result[i] = '\0';
+	return (result);
+}
