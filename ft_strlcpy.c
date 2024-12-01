8d1e5e280f5926518ab2e17ea4e0871cb6e13c22
theo <theo.ubuntu42@gmail.com>
2024-12-01 09:41:02 +0100

    j'ai corrige mon erreur sur ft_strncmp.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -4,7 +4,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
 {
 	size_t	i;
 
-	if (dst == NULL || src == NULL)
+	if (dst == NULL && src == NULL)
 		return (0);
 	i = 0;
 	if (dst == NULL || src == NULL)
